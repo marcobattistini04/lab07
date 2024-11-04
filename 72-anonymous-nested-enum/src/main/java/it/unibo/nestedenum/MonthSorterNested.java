7242cb21a481685c1f58673d1105e473a8d44359
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-11-04 20:13:24 +0100

    Correct version of MonthSortedNested.java ( after an hour spent try debugging without noticing the missing month AUGUSTTTTTTT  :))))))))

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -8,9 +8,10 @@ import java.util.Objects;
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
+
     public static enum Month {
-        GENNAIO(31), FEBBRAIO(28), MARZO(31), APRILE(30), MAGGIO(31), GIUGNO(30), 
-        LUGLIO(31), SETTEMBRE(30), OTTOBRE(31), NOVEMBRE(30), DICEMBRE(31);
+        JANUARY(31), FEBRUARY(28), MARCH(31), APRIL(30), MAY(31), JUNE(30), 
+        JULY(31), AUGUST(31), SEPTEMBER(30), OCTOBER(31), NOVEMBER(30), DECEMBER(31);
 
         private final int monthDays;
 
@@ -25,9 +26,9 @@ public final class MonthSorterNested implements MonthSorter {
             } catch (IllegalArgumentException e) {
                 Month similarity = null;
                 for(final Month montValue : values()) {
-                    if(montValue.toString().toLowerCase().startsWith(str)) {
+                    if(montValue.toString().toLowerCase().startsWith(str.toLowerCase())) {
                         if(similarity != null) {
-                            throw new IllegalStateException("Ambiguous string passed as input.\n" + str + 
+                            throw new IllegalArgumentException("Ambiguous string passed as input.\n" + str + 
                             " could be both " + similarity.toString() + " and " + montValue.toString());
                         }
 
@@ -35,7 +36,7 @@ public final class MonthSorterNested implements MonthSorter {
                     }
                 }
                 if(similarity == null) {
-                    throw new IllegalStateException("Impossible to find any similarity with any month with the string: " + str);
+                    throw new IllegalArgumentException("Impossible to find any similarity with any month with the string: " + str);
                 }
                 return similarity;
             }
@@ -48,19 +49,18 @@ public final class MonthSorterNested implements MonthSorter {
     }
 
     private final static class SortByDays implements Comparator<String> {
-        private final static int POS = 1;
-        private final static int NEG = -1;
-        private final static int ZERO = 0;
+        final static int POS = 1;
+        final static int NEG = -1;
+        final static int ZERO = 0;
         @Override
         public int compare(String o1, String o2) {
             final Month m1 = Month.fromString(o1);
             final Month m2 = Month.fromString(o2);
             if(m1.monthDays > m2.monthDays) {
                 return POS;
-            } else if (m1.monthDays < m2.monthDays) {
+            } else if(m1.monthDays < m2.monthDays) {
                 return NEG;
             }
-
             return ZERO;
         }
         
@@ -72,17 +72,12 @@ public final class MonthSorterNested implements MonthSorter {
     }
 
     private final static class SortByOrder implements Comparator<String> {
-        private final static int POS = 1;
-        private final static int NEG = -1;
         @Override
         public int compare(String o1, String o2) {
             final Month m1 = Month.fromString(o1);
             final Month m2 = Month.fromString(o2);
-            if(m1.ordinal() > m2.ordinal()) {
-                return POS;
-            }
-            return  NEG;
+            return m1.compareTo(m2);
         }
-        
     }
+
 }
