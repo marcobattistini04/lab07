26228b63610d2281b9be91672d5f9fcd8fea3d9b
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-11-04 13:14:53 +0100

    modified the implementation of IterableWithPolicyImpl in order to use the Predicate control. Modified TestIterableWithPolicy in order to use my implementation

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,37 +1,83 @@
 package it.unibo.inner.impl;
 import java.util.Iterator;
 
+
+import it.unibo.inner.api.Predicate;
+
 import it.unibo.inner.api.*;
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
 
-    private T array[];
-    public IterableWithPolicyImpl (T array[]) {
-        this.array = array;
+    private final T array[];
+    private  Predicate<T> pred;
+    public IterableWithPolicyImpl(T[] array) {
+        this(array, new Predicate<T>() {
+            @Override
+            public boolean test(T t) {
+                return true;
+            }
+        });
     }
+
+
+    public IterableWithPolicyImpl (T array[], Predicate<T> pred) {
+        this.array = array;
+        this.pred = pred;
+    }
+
     @Override
     public Iterator<T> iterator() {
-        Iterator<T> iter = new InnerIterator<T>(this.array);
+        Iterator<T> iter = new InnerIterator<T>(this.array, this.pred);
         return iter;
     }
 
     @Override
     public void setIterationPolicy(Predicate<T> filter) {
-    
+        this.pred = filter;
     }
 
-    public static class InnerIterator<T> implements java.util.Iterator<T>{
+    public  String toString() {
+        String str = "[";
+        for( T elem : this.array){
+            str = str + " " + elem;
+        }
+        str = str + "]";
+        return str;
+    }
+
+    public static class InnerIterator<T> implements java.util.Iterator<T> {
         private int arrayCounter = 0;
-        private T array[];
-        public InnerIterator(T array[]) {
+        private final T array[];
+        private final Predicate<T> pred;
+        public InnerIterator(T array[], Predicate<T> pred) {
             this.array = array;
+            this.pred = pred;
         }
         public T next() {
-            return this.array[arrayCounter ++];
+            if(this.array[arrayCounter] == null) {
+                throw new IllegalArgumentException("Out of bound selection");
+            }
+            if(hasNext()) {
+                return this.array[arrayCounter++];
+            }
+            
+            throw new IllegalArgumentException("Out of bound selection");
+            
         } 
 
         public boolean hasNext() {
-            return arrayCounter < this.array.length;
+            while( arrayCounter < this.array.length) {
+                if(this.pred.test(this.array[arrayCounter])) {
+                    return true;
+                }
+                arrayCounter ++;
+            }
+            
+            return false;
         }
     }
+
+    
+
+    
     
 }
