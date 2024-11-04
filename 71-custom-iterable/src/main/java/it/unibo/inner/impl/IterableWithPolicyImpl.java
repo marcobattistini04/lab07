a47f2025ab8dc710885cfa7de5baf148ee4eab41
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-11-04 11:05:17 +0100

    modified TestIterablePlain.java in order to use my first attempt of implementation

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,38 @@
+package it.unibo.inner.impl;
+import java.util.Iterator;
+
+import it.unibo.inner.api.*;
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
+
+    private T array[];
+    public IterableWithPolicyImpl (T array[]) {
+        this.array = array;
+    }
+    @Override
+    public Iterator<T> iterator() {
+        Iterator<T> iter = new InnerIterator<T>(this.array);
+        return iter;
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate<T> filter) {
+    
+    }
+
+    public static class InnerIterator<T> implements java.util.Iterator<T>{
+        private int arrayCounter = 0;
+        private T array[];
+        public InnerIterator(T array[]) {
+            this.array = array;
+        }
+        public T next() {
+            arrayCounter ++;
+            return this.array[arrayCounter];
+        } 
+
+        public boolean hasNext() {
+            return arrayCounter < this.array.length;
+        }
+    }
+    
+}
